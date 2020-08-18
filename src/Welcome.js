0d563f1423047279e826cbd6d43c2534e9cb85e2
rokuirie <akenagibbsherbert6@gmail.com>
2020-08-18 13:24:35 +0300

    modified:   src/Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,7 @@ import Button from './Button';
 function Welcome() {
     return (
         <div>
-            <h2>Welcome to our OLMO!</h2>
+            <h2>Welcome to OLMO!</h2>
             <p>Our goals include the following;</p>
             <App />
             <p>Want to get on board?</p>
