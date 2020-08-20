03e5b27daedc24fddb428fb588879e6ec79494f4
rokuirie <akenagibbsherbert6@gmail.com>
2020-08-20 14:14:47 +0300

    modified:   public/index.html 	modified:   src/Home.js 	modified:   src/Nav.js 	modified:   src/index.css 	added google fonts from fonts.google.com

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import './index.css';
 function Home() {
     return (
         <div className="home-div">
+            <br />
             <h2>This is the home section.</h2>
         </div>
     )
--- src/Nav.js
+++ src/Nav.js
@@ -1,27 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Nav() {
     return (
         <div className="nav-div">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <Link to="/" className="navbar-brand">Home</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <Link to="/about" className="nav-link">About</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <Link to="/services" className="nav-link">Services</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <Link to="/contact" className="nav-link">Contact</Link>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
+                        
                     </ul>
                 </div>
             </nav>
