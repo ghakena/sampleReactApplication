689feac83708384b9e6bda6e6d7949fddde77b0f
rokuirie <akenagibbsherbert6@gmail.com>
2020-08-20 13:08:59 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/Nav.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Home from './Home';
 import About from './About';
 import Services from './Services';
 import Contact from './Contact';
+import Nav from './Nav.js';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
     return(
         <div className="app-div">
-            <h1>This is the Header Section of the App.</h1>
-            <Home />
-            <Services />
-            <About />
-            <Contact />
+            <BrowserRouter>
+                <h1>This is the Header Section of the App.</h1>
+                <Nav />
+                <Home />
+                <Services />
+                <About />
+                <Contact />
+            </BrowserRouter>
+            
         </div>
     )
 }
